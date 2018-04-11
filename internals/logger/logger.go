914e3c718895329b901a642b6fff18522a83d1b8
Alik Khilazhev <alikhil@mail.ru>
2018-04-11 12:07:54 +0300

    Add logger

---
+++ internals/logger/logger.go
@@ -0,0 +1,45 @@
+package logger
+
+import (
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+var (
+	// Trace - Very low level messages
+	Trace *log.Logger
+	// Info - something that is could be useful for
+	Info    *log.Logger
+	Warning *log.Logger
+	Error   *log.Logger
+)
+
+func Init(
+	traceHandle io.Writer,
+	infoHandle io.Writer,
+	warningHandle io.Writer,
+	errorHandle io.Writer) {
+
+	Trace = log.New(traceHandle,
+		"TRACE: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
+
+	Info = log.New(infoHandle,
+		"INFO: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
+
+	Warning = log.New(warningHandle,
+		"WARNING: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
+
+	Error = log.New(errorHandle,
+		"ERROR: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
+}
+
+func init() {
+	Init(ioutil.Discard, os.Stdout, os.Stdout, os.Stderr)
+	Info.Printf("Logger has been initialized")
+}
