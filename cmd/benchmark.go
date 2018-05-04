ad177442bd58861b99d1c0469cd3758bde1e541d
piaxar <ivanpanchenko180@gmail.com>
2018-05-04 17:14:29 +0300

    Data for benchmarking

--- cmd/benchmark.go
+++ cmd/benchmark.go
@@ -14,14 +14,14 @@ import (
 	"github.com/alikhil/TBMS/internals/logger"
 )
 
-type strToParam func(string) *tuple.Tuple
+type strToParam func(string) []*tuple.Tuple
 
 func strToAuthorParam(text string) []*tuple.Tuple {
 	words := strings.Fields(text)
 	name := strings.Join(words[1:], " ")
 	id, err := strconv.Atoi(words[0])
 	if err != nil {
-		panic("Can't convert to int" + words[0])
+		panic("Can't convert author ID to int" + words[0])
 	}
 
 	logger.Info.Printf("Read: " + strconv.Itoa(id) + " -> " + name)
@@ -32,8 +32,31 @@ func strToAuthorParam(text string) []*tuple.Tuple {
 
 }
 
-func scanParamFromFile(filename string) {
+func strToPaperParam(text string) []*tuple.Tuple {
+	words := strings.Fields(text)
+	title := strings.Join(words[1:], " ")
+	id, err := strconv.Atoi(words[0])
+	if err != nil {
+		panic("Can't convert paper ID to int" + words[0])
+	}
 
+	logger.Info.Printf("Read: " + strconv.Itoa(id) + " -> " + title)
+	params := []*tuple.Tuple{
+		tuple.NewTupleFromItems("id", id),
+		tuple.NewTupleFromItems("title", title)}
+	return params
+}
+
+func scanParamFromFile(filepath string, fn strToParam) {
+	inFile, _ := os.Open(filepath)
+	defer inFile.Close()
+	scanner := bufio.NewScanner(inFile)
+	scanner.Split(bufio.ScanLines)
+
+	for scanner.Scan() {
+		params := fn(scanner.Text())
+		logger.Info.Printf("%v", (*params[1]).Get(1))
+	}
 }
 
 func runBenchmark() {
@@ -42,17 +65,10 @@ func runBenchmark() {
 	api.Init(re)
 	logger.Info.Printf("from benchmark")
 
-	var path = "./authors.in"
-
-	inFile, _ := os.Open(path)
-	defer inFile.Close()
-	scanner := bufio.NewScanner(inFile)
-	scanner.Split(bufio.ScanLines)
-
-	for scanner.Scan() {
-		params := strToAuthorParam(scanner.Text())
-		logger.Info.Printf("%v", (*params[1]).Get(1))
-	}
+	var path = "../test_data/1000/authors.in"
+	scanParamFromFile(path, strToAuthorParam)
+	path = "../test_data/1000/papers.in"
+	scanParamFromFile(path, strToPaperParam)
 
 	// var author = api.createNode()   // (type = "Author", properties = {id: 1; name: "andrew NG"})
 	// var article1 = api.createNode() // (type = "Paper", properties = {id: 10; title: "Bitcons for breakfast"})
