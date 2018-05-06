017a0d5fbea2ea7cabeb86a46138c1dd82f8ded4
Alik Khilazhev <alikhil@mail.ru>
2018-05-06 20:43:56 +0300

    Add cache & dfs tests

--- internals/io/io_test.go
+++ internals/io/io_test.go
@@ -2,6 +2,8 @@ package io
 
 import (
 	"bytes"
+	"fmt"
+	"github.com/alikhil/distributed-fs/utils"
 	"testing"
 )
 
@@ -31,6 +33,43 @@ func readWriteFile(c IO, t *testing.T) {
 	}
 }
 
+func completeTest(c IO, t *testing.T) {
+	fname := "nodes.store"
+	var recordSize byte = 13
+	const n byte = 19
+	var m = n * recordSize
+
+	data := make([]byte, m)
+	var offset int32
+	for i := byte(0); i < m; i++ {
+		data[i] = i
+	}
+
+	_ = c.CreateFile(fname)
+	defer c.DeleteFile(fname)
+
+	c.WriteBytes(fname, offset, &data)
+
+	orders := [n]int32{0, 2, 1, 4, 3, 10, 9, 8, 13, 4, 2, 8, 1, 15, 14, 4, 5, 10, 3}
+
+	for _, i := range orders {
+		localOffset := i * int32(recordSize)
+		to := localOffset + int32(recordSize)
+		dt, ok := c.ReadBytes(fname, localOffset, int32(recordSize))
+		if !ok {
+			t.Fatalf("failed to read range of bytes %d: %d", localOffset, to)
+		}
+
+		rdt := data[localOffset:to]
+
+		if !bytes.Equal(dt, rdt) {
+			t.Errorf("expected %v but get %v", rdt, dt)
+		}
+
+	}
+
+}
+
 func TestLocalIOReadWrite(t *testing.T) {
 	readWriteFile(LocalIO{}, t)
 }
@@ -39,6 +78,9 @@ func TestLocalIOCreateFile(t *testing.T) {
 	createFile(LocalIO{}, t)
 }
 
+func TestLocalIOComplete(t *testing.T) {
+	completeTest(LocalIO{}, t)
+}
 func TestCacheReadWrite(t *testing.T) {
 	var mapa = map[string]int32{
 		"nodes.store":             13,
@@ -71,4 +113,87 @@ func TestCacheCreateFile(t *testing.T) {
 	cache := LRUCache{}
 	cache.Init(LocalIO{}, &mapa, 2)
 	createFile(&cache, t)
+}
+
+func TestCacheComplete(t *testing.T) {
+	var mapa = map[string]int32{
+		"nodes.store":             13,
+		"labels.store":            9,
+		"labelsStrings.store":     21,
+		"relationships.store":     34,
+		"properties.store":        14,
+		"strings.store":           64,
+		"inuse.store":             11,
+		"propertykeys.store":      21,
+		"relationshiptypes.store": 34,
+	}
+	cache := LRUCache{}
+	cache.Init(LocalIO{}, &mapa, 2)
+	completeTest(&cache, t)
+}
+
+func TestDistributeFSReadWrite(t *testing.T) {
+	var mapa = map[string]int32{
+		"nodes.store": 13,
+	}
+
+	client, ok := utils.GetRemoteClient(fmt.Sprintf("%s:5001", utils.GetIPAddress()))
+	if !ok {
+		t.Error("failed to connect to remote client")
+	}
+	dfs := utils.DFSClient{Client: client}
+	dfs.InitRecordMappings(&mapa)
+
+	readWriteFile(&dfs, t)
+}
+
+func TestDistributeFSComplete(t *testing.T) {
+	var mapa = map[string]int32{
+		"nodes.store": 13,
+	}
+
+	client, ok := utils.GetRemoteClient(fmt.Sprintf("%s:5001", utils.GetIPAddress()))
+	if !ok {
+		t.Error("failed to connect to remote client")
+	}
+	dfs := utils.DFSClient{Client: client}
+	dfs.InitRecordMappings(&mapa)
+
+	completeTest(&dfs, t)
+}
+
+func TestCacheWithDistributeFSReadWrite(t *testing.T) {
+	var mapa = map[string]int32{
+		"nodes.store": 13,
+	}
+
+	client, ok := utils.GetRemoteClient(fmt.Sprintf("%s:5001", utils.GetIPAddress()))
+	if !ok {
+		t.Error("failed to connect to remote client")
+	}
+	dfs := utils.DFSClient{Client: client}
+	dfs.InitRecordMappings(&mapa)
+
+	cache := LRUCache{}
+	cache.Init(&dfs, &mapa, 5)
+
+	readWriteFile(&cache, t)
+}
+
+func TestCacheWithDistributeFSComplete(t *testing.T) {
+	var mapa = map[string]int32{
+		"nodes.store": 13,
+	}
+
+	client, ok := utils.GetRemoteClient(fmt.Sprintf("%s:5001", utils.GetIPAddress()))
+	if !ok {
+		t.Error("failed to connect to remote client")
+	}
+	dfs := utils.DFSClient{Client: client}
+	dfs.InitRecordMappings(&mapa)
+
+	cache := LRUCache{}
+	cache.Init(&dfs, &mapa, 5)
+
+	completeTest(&cache, t)
 }
